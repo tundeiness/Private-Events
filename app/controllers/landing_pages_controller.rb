ff5d24970cba7ef6e94f69a6dc9e24528d3e7270
tundeiness <tundeiness@yahoo.co.uk>
2019-09-17 15:33:12 +0100

    Add a Landing Pages controller

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,13 @@
+class LandingPagesController < ApplicationController
+  def home
+  end
+
+  def event
+  end
+
+  def signin
+  end
+
+  def signup
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'landing_pages/home'
+  get 'landing_pages/event'
+  get 'landing_pages/signin'
+  get 'landing_pages/signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
