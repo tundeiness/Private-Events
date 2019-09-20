25c3d6a2255d7f6c2e49adf6f2d3a1b0d2cee0f7
tundeiness <tundeiness@yahoo.co.uk>
2019-09-20 11:46:57 +0100

    make user model with secure password

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_143707) do
+ActiveRecord::Schema.define(version: 2019_09_20_102717) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) do
-    @user = User.create(name: 'Another User', email: 'auser@example.com ')
+  subject(:user) do
+   described_class.new name: name, email: email, password: password, password_confirmation: password_confirmation
   end
 
-  context 'name and email is not present' do
-    subject { User.create(name: ' ', email: ' ') }
-    #byebug
-    it 'is not valid' do
-      expect(subject).to_not be_valid
-    end
+  let(:name) {'Katherine'}
+  let(:email) {'Kathy@yahoo.co.uk'}
+  let(:password){"foobar"}
+  let(:password_confirmation){"foobar"}
+
+  it{is_expected.to be_valid}
+
+  context 'with a name that is blank' do
+    let(:name){' '}
+    it{ is_expected.to_not be_valid }
+  end
+
+  context 'with an email that is blank' do
+    let(:email){' '}
+    it{ is_expected.to_not be_valid }
   end
 
   context 'all fields present' do
-    subject { User.create(name: 'Another User', email: 'auser@example.com ') }
-    #byebug
-    it 'is valid' do
-      expect(subject).to be_valid
-    end
+    let(:name) {'Katherine'}
+    let(:email) {'Kathy@yahoo.co.uk'}
+
+    it{is_expected.to be_valid}
   end
 
-  context 'name length' do
-    it 'should not be longer than 50 characters' do
-      @user.name = 'a' * 51
-      expect(@user).to_not be_valid
-   end
+  context 'name with over 20 characters' do
+    let(:name){'Antidisestermentarialism...really really long name'}
+    it{is_expected.to_not be_valid}
   end
 
-  context 'email length' do
-    it 'should not be longer than 255 characters' do
-      @user.email = "a" * 244 + "@example.com"
-      expect(@user).to_not be_valid
+  context 'name with less than 4 characters' do
+    let(:name){'xte'}
+    it{is_expected.to_not be_valid}
+  end
+
+  context 'email with over 255 characters' do
+      let(:email){"a" * 244 + "@example.com"}
+      it{ is_expected.to_not be_valid }
+  end
+
+  context 'email format validation' do
+    let(:email){%w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]}
+      it{ is_expected.to_not be_valid }
     end
+
+  context 'with a password (nonblank)' do
+    let(:password){" " * 6}
+    it{ is_expected.to_not be_valid }
+  end
+
+  context 'password with a minimum length' do
+    let(:password_confirmation){"a" * 5}
+    it{ is_expected.to_not be_valid }
   end
 end
