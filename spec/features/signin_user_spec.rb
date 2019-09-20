36833865a8d59943567c5f3aed5bdf9666cdffce
tundeiness <tundeiness@yahoo.co.uk>
2019-09-20 23:34:47 +0100

    refactor signin spec

--- spec/features/signin_user_spec.rb
+++ spec/features/signin_user_spec.rb
@@ -15,14 +15,14 @@ RSpec.feature User, type: :feature do
 
     #fill_in :password, with: '123456789'
     find('#session_email').set('test@gmail.com')
-    #find('#session_password').set('123456789')
+    find('#session_password').set('123456789')
     click_button('Sign in')
     #expect(page).to redirect_to(new_path(@user))
     #expect(page).to have_current_path(user_path(@user))
-    page.find("#session_email").click
+    #page.find("#session_email").click
    # page.find("#session_password").click
      mock_user.reload
     expect(mock_user.email).to eq(mock_user.email)
-    #expect(user11.password).to eq(user11.password)
+    expect(mock_user.password).to eq(mock_user.password)
   end
 end
