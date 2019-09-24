2ca402dd7a8de3a764c4ee71a48b4c660699f596
tundeiness <tundeiness@yahoo.co.uk>
2019-09-24 12:31:24 +0100

    test for valid user, invalid user, valid password, and invalid password during sign in

--- Gemfile
+++ Gemfile
@@ -52,7 +52,7 @@ group :development, :test do
   # The RSpec testing framework
   gem 'rspec-rails', '~> 3.8'
   gem 'pry'
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
   gem 'faker'
   # Capybara, the library that allows us to interact with the browser using Ruby
   gem 'capybara'
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature "Signup", type: :feature do
+  feature 'User signs up' do
+    scenario 'with name only' do
+      @mock_user = User.create(name:'test',
+        name: 'Michael Hartl',
+        email: 'mhartl@example.com',
+        password: 'foobar')
+      visit signin_path
+      fill_in 'Email', with: @mock_user.email
+      fill_in 'Password', with: @mock_user.password
+      click_button('Sign in')
+
+      expect(page).to have_css '.user_info'
+      expect(page).to have_content `Welcome" #{@mock_user.name}`
+    end
+
+    scenario 'with email only' do
+      @mock_user = User.create(name:'test',
+        name: 'Michael Hartl',
+        email: 'mhartl@example.com',
+        password: 'foobar')
+      visit signin_path
+      fill_in 'Email', with: @mock_user.email
+      fill_in 'Password', with: @mock_user.password
+      click_button('Sign in')
+
+      expect(page).to have_css '.user_info'
+      expect(page).to have_content `Welcome" #{@mock_user.name}`
+    end
+
+    scenario 'with name email and password' do
+      @mock_user = User.create(name:'test',
+        name: 'Michael Hartl',
+        email: 'mhartl@example.com',
+        password: 'foobar')
+      visit signin_path
+      fill_in 'Email', with: @mock_user.email
+      fill_in 'Password', with: @mock_user.password
+      click_button('Sign in')
+
+      expect(page).to have_css '.user_info'
+      expect(page).to have_content `Welcome" #{@mock_user.name}`
+    end
+
+    scenario 'with name email password and confirmation' do
+      @mock_user = User.create(name:'test',
+        name: 'Michael Hartl',
+        email: 'mhartl@example.com',
+        password: 'foobar')
+      visit signin_path
+      fill_in 'Email', with: @mock_user.email
+      fill_in 'Password', with: @mock_user.password
+      click_button('Sign in')
+
+      expect(page).to have_css '.user_info'
+      expect(page).to have_content `Welcome" #{@mock_user.name}`
+    end
+  end
+end
+
+
