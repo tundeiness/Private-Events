4c954801f59fffa9b7082242d108cf8ea64a9105
tundeiness <tundeiness@yahoo.co.uk>
2019-09-24 13:59:49 +0100

    complete feature test for signup and signin forms

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,63 +2,73 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.feature "Signup", type: :feature do
+
   feature 'User signs up' do
-    scenario 'with name only' do
-      @mock_user = User.create(name:'test',
-        name: 'Michael Hartl',
-        email: 'mhartl@example.com',
-        password: 'foobar')
-      visit signin_path
-      fill_in 'Email', with: @mock_user.email
+    scenario 'with name, password and password confirmation only' do
+      @mock_user = User.create(
+        name: 'mark Greyhound',
+        email: 'mg@example.com',
+        password: 'foobaz',
+        password_confirmation: 'foobaz')
+      visit signup_path
+      fill_in 'Name', with: @mock_user.name
+      fill_in 'Email', with: ' '
       fill_in 'Password', with: @mock_user.password
-      click_button('Sign in')
+      fill_in 'Confirmation', with: @mock_user.password_confirmation
+      click_button('Sign up')
 
-      expect(page).to have_css '.user_info'
-      expect(page).to have_content `Welcome" #{@mock_user.name}`
+      expect(page).to have_css '.alert-danger'
     end
 
-    scenario 'with email only' do
-      @mock_user = User.create(name:'test',
-        name: 'Michael Hartl',
-        email: 'mhartl@example.com',
-        password: 'foobar')
-      visit signin_path
+    scenario 'with email, password and password confirmation only' do
+      @mock_user = User.create(
+        name: 'mark Greyhound',
+        email: 'mg@example.com',
+        password: 'foobaz',
+        password_confirmation: 'foobaz')
+      visit signup_path
+      fill_in 'Name', with: ' '
       fill_in 'Email', with: @mock_user.email
       fill_in 'Password', with: @mock_user.password
-      click_button('Sign in')
+      fill_in 'Confirmation', with: @mock_user.password_confirmation
+      click_button('Sign up')
 
-      expect(page).to have_css '.user_info'
-      expect(page).to have_content `Welcome" #{@mock_user.name}`
+      expect(page).to have_css '.alert-danger'
     end
 
-    scenario 'with name email and password' do
-      @mock_user = User.create(name:'test',
-        name: 'Michael Hartl',
-        email: 'mhartl@example.com',
-        password: 'foobar')
-      visit signin_path
+    scenario 'with name, email, and password' do
+      @mock_user = User.create(
+        name: 'mark Greyhound',
+        email: 'mg@example.com',
+        password: 'foobaz',
+        password_confirmation: 'foobaz')
+      visit signup_path
+      fill_in 'Name', with: @mock_user.email
       fill_in 'Email', with: @mock_user.email
       fill_in 'Password', with: @mock_user.password
-      click_button('Sign in')
+      fill_in 'Confirmation', with: ' '
+      click_button('Sign up')
 
-      expect(page).to have_css '.user_info'
-      expect(page).to have_content `Welcome" #{@mock_user.name}`
+      expect(page).to have_css '.alert-danger'
     end
 
-    scenario 'with name email password and confirmation' do
-      @mock_user = User.create(name:'test',
-        name: 'Michael Hartl',
-        email: 'mhartl@example.com',
-        password: 'foobar')
-      visit signin_path
+    scenario 'with name, email, password, and password confirmation' do
+      @mock_user = User.create(
+        name: 'mark Greyhound',
+        email: 'mg@example.com',
+        password: 'foobaz',
+        password_confirmation: 'foobaz')
+      visit signup_path
+      fill_in 'Name', with: @mock_user.email
       fill_in 'Email', with: @mock_user.email
       fill_in 'Password', with: @mock_user.password
-      click_button('Sign in')
+      fill_in 'Confirmation', with: @mock_user.password_confirmation
+      click_button('Sign up')
 
-      expect(page).to have_css '.user_info'
-      expect(page).to have_content `Welcome" #{@mock_user.name}`
+      expect(page).to have_css '.alert-success'
     end
   end
 end
 
 
+#user_password_confirmation
