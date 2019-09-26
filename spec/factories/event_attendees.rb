9e9641af00abf4cdfb4cc54821983390b34db5c7
tundeiness <tundeiness@yahoo.co.uk>
2019-09-26 14:57:16 +0100

    replace factoryGirl

--- spec/factories/event_attendees.rb
+++ spec/factories/event_attendees.rb
@@ -1,6 +1,16 @@
-FactoryGirl.define do
+#FactoryGirl.define do
+#  factory :event_attendee do
+#    attendee_id 1
+#    attended_event_id 1
+#  end
+#end
+
+FactoryBot.define do
   factory :event_attendee do
-    attendee_id 1
-    attended_event_id 1
+    association :attendee, factory: [:user, :event]
+  end
+
+  factory :random_event_attendee, class: EventAttendee do
+    association :attendee, factory: [:user, :event]
   end
 end
