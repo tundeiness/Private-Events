a124f1c66210d4c7533dcad6d5308ebd02e48b86
tundeiness <tundeiness@yahoo.co.uk>
2019-09-26 15:36:56 +0100

    fix change issue in spec controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-<<<<<<< HEAD
-
-=======
   describe 'GET #new' do
     it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
->>>>>>> 0d36ce64fa941d89f6dc182548d194166f04275a
 end
