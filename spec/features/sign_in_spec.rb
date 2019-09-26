fd84624e8e90c11ef11aaebf05826b3c769b5867
tundeiness <tundeiness@yahoo.co.uk>
2019-09-26 15:11:04 +0100

    fix conflict

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.feature 'SignIns', type: :feature do
-<<<<<<< HEAD
-
-=======
->>>>>>> 0d36ce64fa941d89f6dc182548d194166f04275a
   feature 'User signs in' do
     scenario 'with registered email' do
       @mock_user = User.create(name: 'Michael Hartl',
@@ -17,8 +13,6 @@ RSpec.feature 'SignIns', type: :feature do
       click_button('Sign in')
 
       expect(page).to have_css '.user_info'
-<<<<<<< HEAD
-=======
     end
 
     scenario 'with an unregistered email' do
@@ -46,37 +40,6 @@ RSpec.feature 'SignIns', type: :feature do
       click_button('Sign in')
 
       expect(page).to have_content 'Invalid email/password combination'
->>>>>>> event-attending
     end
-<<<<<<< HEAD
-
-    scenario 'with an unregistered email' do
-      visit signin_path
-      fill_in 'Email', with: 'test@example.com'
-      fill_in 'Password', with: '123456789'
-      click_button('Sign in')
-
-      expect(page).to have_content 'Invalid email/password combination'
-    end
-
-    scenario 'with a valid user and without a password' do
-      visit signin_path
-      fill_in 'Email', with: 'mhartl@example.com'
-      fill_in 'Password', with: ' '
-      click_button('Sign in')
-
-      expect(page).to have_content 'Invalid email/password combination'
-    end
-
-    scenario 'without a user and without a password' do
-      visit signin_path
-      fill_in 'Email', with: ' '
-      fill_in 'Password', with: ' '
-      click_button('Sign in')
-
-      expect(page).to have_content 'Invalid email/password combination'
-    end
-=======
->>>>>>> 0d36ce64fa941d89f6dc182548d194166f04275a
   end
 end
